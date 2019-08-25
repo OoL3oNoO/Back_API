3ffdf01208c5cb8f70c53b9549dfff0d8000e931
Damien <dam.haultcoeur@gmail.com>
2019-08-25 12:27:07 +0200

    modif

--- controllers/entreprises.js
+++ controllers/entreprises.js
@@ -61,11 +61,10 @@ entreprise.updateEntreprise = (entreprise, id) => {
       entreprise.entstatut,
       entreprise.entsiret,
       entreprise.contacts_id_contact,
-     id
-      
+      id
     ];
     const query =
-      'UPDATE entreprises SET entname = ?, entadress = ?, entzip = ?, entcity = ?, entphone = ?, entmail = ? entstatut = ?, entsiret = ?, contacts_id_contact = ? WHERE identreprises = ?';
+      'UPDATE entreprises SET entname = ?, entadress = ?, entzip = ?, entcity = ?, entphone = ?, entmail = ?, entstatut = ?, entsiret = ?, contacts_id_contact = ? WHERE identreprises = ?';
       dbConnect.query(query, params, (err, res) => {
       if (err) return reject(err);
       resolve(res);
