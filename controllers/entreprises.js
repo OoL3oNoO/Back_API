5a81b7b2bf3cacd723dfee53c89231554c06b9e0
Damien <dam.haultcoeur@gmail.com>
2019-09-19 20:13:13 +0200

    essai de sécurisation des requetes

--- controllers/entreprises.js
+++ controllers/entreprises.js
@@ -1,5 +1,5 @@
-const dbConnect = require('../dbConnect')
-const express = require('express');
+const dbConnect = require("../dbConnect");
+const express = require("express");
 
 //Requêtes sur la table entreprise.
 let entreprise = {};
@@ -7,21 +7,30 @@ let entreprise = {};
 //Query sur la table entreprise pour toutes les récupèrer
 entreprise.findAllEntreprises = () => {
   return new Promise((resolve, reject) => {
-    dbConnect.query('SELECT * FROM entreprises LEFT JOIN contacts ON entreprises.identreprises = contacts.entreprises_identreprises  ', (err, res) => {
-      if (err) return reject(err);
-      return resolve(res);
-    });
-    
+    var query =
+      "SELECT * FROM entreprises LEFT JOIN contacts ON entreprises.identreprises = contacts.entreprises_identreprises";
+    dbConnect.query(
+      query,
+      // "SELECT * FROM entreprises LEFT JOIN contacts ON entreprises.identreprises = contacts.entreprises_identreprises  ",
+      (err, res) => {
+        if (err) return reject(err);
+        return resolve(res);
+      }
+    );
   });
 };
 
 //Query sur la table entreprise pour récupérer une entreprise avec un paramètre id.
 entreprise.onlyOneEntreprise = id => {
   return new Promise((resolve, reject) => {
-    dbConnect.query('SELECT DISTINCT * FROM entreprises LEFT JOIN contacts ON entreprises.identreprises = contacts.entreprises_identreprises WHERE entreprises.identreprises = ?', [id], (err, res) => {
-      if (err) return reject(err);
-      return resolve(res);
-    });
+    dbConnect.query(
+      "SELECT DISTINCT * FROM entreprises LEFT JOIN contacts ON entreprises.identreprises = contacts.entreprises_identreprises WHERE entreprises.identreprises = ?",
+      [id],
+      (err, res) => {
+        if (err) return reject(err);
+        return resolve(res);
+      }
+    );
   });
 };
 // entreprise.onlyOneEntreprise = id => {
@@ -32,7 +41,6 @@ entreprise.onlyOneEntreprise = id => {
 //     });
 //   });
 // };
-
 
 //Query sur la table entreprise pour créer une entreprise.
 entreprise.newEntreprise = entreprise => {
@@ -45,11 +53,11 @@ entreprise.newEntreprise = entreprise => {
       entreprise.entphone,
       entreprise.entmail,
       entreprise.entstatut,
-      entreprise.entsiret,
+      entreprise.entsiret
     ];
     const query =
-      'INSERT INTO entreprises (entname, entadress, entzip, entcity, entphone, entmail, entstatut, entsiret) VALUES (?, ?, ?, ?, ?, ?, ?,?)';
-      dbConnect.query(query, params, (err, res) => {
+      "INSERT INTO entreprises (entname, entadress, entzip, entcity, entphone, entmail, entstatut, entsiret) VALUES (?, ?, ?, ?, ?, ?, ?,?)";
+    dbConnect.query(query, params, (err, res) => {
       if (err) return reject(err);
       resolve(res);
     });
@@ -71,8 +79,8 @@ entreprise.updateEntreprise = (entreprise, id) => {
       id
     ];
     const query =
-      'UPDATE entreprises SET entname = ?, entadress = ?, entzip = ?, entcity = ?, entphone = ?, entmail = ?, entstatut = ?, entsiret = ? WHERE identreprises = ?';
-      dbConnect.query(query, params, (err, res) => {
+      "UPDATE entreprises SET entname = ?, entadress = ?, entzip = ?, entcity = ?, entphone = ?, entmail = ?, entstatut = ?, entsiret = ? WHERE identreprises = ?";
+    dbConnect.query(query, params, (err, res) => {
       if (err) return reject(err);
       resolve(res);
     });
@@ -82,10 +90,14 @@ entreprise.updateEntreprise = (entreprise, id) => {
 //Query sur la table entreprise pour supprimer une entreprise avec un paramètre id.
 entreprise.deleteOneEntreprise = id => {
   return new Promise((resolve, reject) => {
-    dbConnect.query('DELETE FROM entreprises WHERE identreprises = ?', [id], (err, res) => {
-      if (err) return reject(err);
-      resolve(res);
-    });
+    dbConnect.query(
+      "DELETE FROM entreprises WHERE identreprises = ?",
+      [id],
+      (err, res) => {
+        if (err) return reject(err);
+        resolve(res);
+      }
+    );
   });
 };
 
